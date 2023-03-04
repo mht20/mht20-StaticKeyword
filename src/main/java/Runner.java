8a979bcc853f157c6fb8a7c675609c98c0aae45f
mht20 <mht252028@gmail.com>
2023-03-04 12:51:40 +0000

    Static Variable

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -22,6 +22,12 @@ public class Runner {
      * @param in the String value you should change global_var to in.
      */
     public void changeClassVar(String in){
+        Class.global_var= in;
+
+         
+
+    
+        
 
     }
 }
